f01c6c7710a9d84a440870f6a9e4666be71a03f4
moha-dev94 <ennadifima@gmail.com>
2026-04-01 17:27:22 +0200

    Update root element content with new message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,1 @@
-document.getElementById('root').innerHTML = '<h1>Interface React connectée au Backend Flask</h1>';
+document.getElementById('root').innerHTML = "<h1>🚀 Interface React opérationnelle !</h1><p>Ennadifi - Projet MAE 2026</p>";
